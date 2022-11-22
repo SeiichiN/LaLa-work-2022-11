1dc2341b833adc08860d27522579e280cb2f8b64
SeiichiN <billie175@gmail.com>
2022-11-22 13:14:17 +0900

    11月22日、午前中まで

---
+++ rpg/src/p307/Main.java
@@ -0,0 +1,9 @@
+package p307;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ rpg/src/world/Matango.java
@@ -0,0 +1,6 @@
+package world;
+
+public class Matango {
+	int hp;
+	final int LEVEL = 10;
+}
