ebd9211aa5493d664f1dbe50e4071199744ebe10
SeiichiN <billie175@gmail.com>
2022-11-25 16:32:30 +0900

    RunInterfaceを実装(笑)

--- rpg/src/world/FlyingMonster.java
+++ rpg/src/world/FlyingMonster.java
@@ -1,6 +1,7 @@
 package world;
 
-public abstract class FlyingMonster extends Monster {
+public abstract class FlyingMonster extends Monster
+						implements RunInterface {
 	public void run() {
 		System.out.println("バサバサ飛んで逃げる");
 	}
--- rpg/src/world/WalkingMonster.java
+++ rpg/src/world/WalkingMonster.java
@@ -1,6 +1,7 @@
 package world;
 
-public abstract class WalkingMonster extends Monster {
+public abstract class WalkingMonster extends Monster
+							implements RunInterface {
 	public void run() {
 		System.out.println("トコトコ走って逃げる");
 	}
