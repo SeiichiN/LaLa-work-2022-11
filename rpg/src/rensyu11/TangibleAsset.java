13c572120918978f25d36fc5e9ae103287bfcbf6
SeiichiN <billie175@gmail.com>
2022-11-29 11:55:10 +0900

    11月29日12時現在

---
+++ rpg/src/rensyu11/TangibleAsset.java
@@ -0,0 +1,12 @@
+package rensyu11;
+
+public abstract class TangibleAsset extends Asset {
+	String color;
+
+	public TangibleAsset(String name, int price, String color) {
+		super(name, price);
+		this.color = color;		
+	}
+	public String getColor() {return this.color; }
+
+}
